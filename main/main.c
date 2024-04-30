4bb725e401d7f04286d11c1dbeb69dd1030d4cd6
Cota <brandon.cota@uabc.edu.mx>
2024-04-29 21:51:46 -0700

    Initial comit

---
+++ main/main.c
@@ -0,0 +1,229 @@
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <esp_http_server.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include <string.h>
+#include "esp_eth.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#include "esp_netif.h"
+#include "esp_tls.h"
+
+
+#define EXAMPLE_ESP_WIFI_SSID      "softAP_SE"
+#define EXAMPLE_ESP_WIFI_PASS      "12345678"
+#define EXAMPLE_MAX_STA_CONN       10
+
+//LED
+#define GPIO_LED 2
+#define ON 1
+#define OFF 0
+
+uint8_t toggle = 0;
+
+//I2C sensor
+#define WHO_AM_I 0x75
+#define POW_MANG 0x6B
+#define POW_MANG2 0x6C
+#define TEMP_OUT 0x41
+
+#define BUFFER_SIZE_READ 10
+
+static const char *TAG = "softAP_WebServer";
+static const char *TAG_I = "I2C";
+
+esp_err_t gpio_init();
+void toggle_led();
+
+esp_err_t gpio_init()
+{
+	gpio_set_direction(2, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_LED, OFF);
+
+	return ESP_OK;
+}
+
+void toggle_led()
+{
+    //static uint8_t toggle = 0;
+
+    toggle = !toggle;
+    (toggle == 1) ? gpio_set_level(GPIO_LED, ON) : gpio_set_level(GPIO_LED, OFF);
+}
+
+/* An HTTP GET handler */
+static esp_err_t index_get_handler(httpd_req_t *req)
+{
+    char*  buf;
+    size_t buf_len;
+    int formattedStrResult = 0;
+    uint16_t sensor_value = 0;
+    uint8_t led_state = 0;
+
+    extern unsigned char view_start[] asm("_binary_view_html_start");
+    extern unsigned char view_end[] asm("_binary_view_html_end");
+    size_t view_len = view_end - view_start;
+    char viewHtml[view_len], led_s[5] = "\0";
+
+    memcpy(viewHtml, view_start, view_len);
+
+    //ESP_LOGI(TAG, "URI: %d", view_len);
+
+
+    /* Get header value string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        /* Copy null terminated value string into buffer */
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+        }
+        free(buf);
+    }
+
+    /* Read URL query string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found URL query => %s", buf);
+            char param[32];
+            /* Get value of expected key from query string */
+            if (httpd_query_key_value(buf, "query1", param, sizeof(param)) == ESP_OK) {
+                ESP_LOGI(TAG, "Found URL query parameter => query1=%s", param);
+
+                if (strcmp(param, "0xD") == 0) {
+                    toggle_led();
+                }
+                else if (strcmp(param, "0xE") == 0) {
+                    led_state = 1;
+                    if(toggle == 0) strcpy(led_s, "OFF");
+                    else strcpy(led_s, "ON");
+                }
+            }
+        }
+        free(buf);
+    }
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    //const char* resp_str = (const char*) req->user_ctx;
+    //httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+
+    //send responce with html file
+    httpd_resp_set_type(req, "text/html");
+
+    char *viewHtmlUpdated;
+    formattedStrResult = asprintf(&viewHtmlUpdated, viewHtml, sensor_value ? "ON" : "N/A", led_state ? led_s : "N/A");
+
+    if (formattedStrResult > 0)
+    {
+        httpd_resp_send(req, viewHtmlUpdated, view_len);
+        free(viewHtmlUpdated);
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Error updating variables");
+        httpd_resp_send(req, viewHtml, view_len);
+    }
+
+    /* After sending the HTTP response the old HTTP request
+     * headers are lost. Check if HTTP request headers can be read now. */
+    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
+        ESP_LOGI(TAG, "Request headers lost");
+    }
+    return ESP_OK;
+}
+
+
+static const httpd_uri_t indexx = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = index_get_handler,
+    .user_ctx  = "This is an index."
+};
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    // Iniciar el servidor httpd 
+    ESP_LOGI(TAG, "Iniciando el servidor en el puerto: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Manejadores de URI
+        ESP_LOGI(TAG, "Registrando manejadores de URI");
+        httpd_register_uri_handler(server, &indexx);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "estacion %X:%X:%X:%X:%X:%X se unio, AID=%d", event->mac[5], event->mac[4],event->mac[3],event->mac[2],event->mac[1],event->mac[0], event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "estacion %X:%X:%X:%X:%X:%X se desconecto, AID=%d", event->mac[5], event->mac[4],event->mac[3],event->mac[2],event->mac[1],event->mac[0], event->aid);
+    }
+}
+
+esp_err_t wifi_init_softap(void)
+{
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "Inicializacion de softAP terminada. SSID: %s password: %s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    return ESP_OK;
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(gpio_init());
+
+    httpd_handle_t server = NULL;
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    esp_netif_init();
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_LOGI(TAG, "init softAP");
+    ESP_ERROR_CHECK(wifi_init_softap());
+
+    server = start_webserver();
+}
+
+
